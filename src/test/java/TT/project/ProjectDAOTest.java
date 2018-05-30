2a906e30d38a294bec1ccb852067a27bebda6280
lminggvick <lminggvick@naver.com>
2018-05-30 22:04:41 +0900

    test3

--- src/test/java/TT/project/ProjectDAOTest.java
+++ src/test/java/TT/project/ProjectDAOTest.java
@@ -1,4 +1,4 @@
-package TT.project;
+/*package TT.project;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -231,3 +231,4 @@ public class ProjectDAOTest {
     logger.debug("" + factory.updateActivity(card, "Old", "New"));
   }
 }
+*/
